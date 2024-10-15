7b2d2e4f6ca2e96cb76f12dc60109700a8b58aac
JuliusDgenius <ibejulius1@gmail.com>
2024-10-15 22:03:22 +0100

    add task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -15,4 +15,4 @@ async def async_generator() -> Generator[float, None, None]:
     """Yield a number between 0 and 10."""
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.uniform(0, 10)
